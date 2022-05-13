e0f417589994e82980fe1d3c2b259ff4b0cf73ea
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-05-13 20:59:20 +0000

    removed the check for overflow (not portable)

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,44 +6,16 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 21:11:56 by troberts          #+#    #+#             */
-/*   Updated: 2022/04/19 22:19:16 by troberts         ###   ########.fr       */
+/*   Updated: 2022/05/13 22:13:26 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	checkoverflow(size_t a, size_t b)
-{
-	size_t	sizetmax;
-	size_t	sizetmin;
-
-	sizetmax = SIZE_MAX;
-	sizetmin = 0;
-	if (a > (sizetmax / b))
-		return (1);
-	else if (a < (sizetmin / b))
-		return (1);
-	else
-		return (0);
-}
-
-static void	*ft_memalloc_static(size_t size)
-{
-	void	*ptr;
-
-	ptr = (void *)malloc(size);
-	if (ptr == NULL)
-		return (NULL);
-	ft_bzero(ptr, size);
-	return (ptr);
-}
-
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	if (checkoverflow(nmemb, size) == 1)
-		return (NULL);
-	ptr = ft_memalloc_static(nmemb * size);
+	ptr = ft_memalloc(nmemb * size);
 	return (ptr);
 }
