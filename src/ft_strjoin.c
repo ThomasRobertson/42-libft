d171e153225abee254e9bcb3e039616436eabbd0
Thomas ROBERTSON <troberts@e3r8p3.clusters.42paris.fr>
2022-08-30 12:42:31 +0200

    refactor: remove use of strnew

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 01:45:26 by troberts          #+#    #+#             */
-/*   Updated: 2022/06/24 12:18:35 by troberts         ###   ########.fr       */
+/*   Updated: 2022/08/11 13:30:07 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,18 @@ static char	*ft_strcpy_static(char *dest, const char *src)
 	return (dest);
 }
 
-static char	*ft_strnew_static(size_t size)
-{
-	char	*str;
-
-	str = malloc(sizeof(*str) * size + 1);
-	if (str == NULL)
-		return (NULL);
-	while (size > 0)
-	{
-		str[size] = 0;
-		size--;
-	}
-	str[0] = 0;
-	return (str);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	len;
 	char	*str;
 	char	*ptr;
 
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
 	len = ft_strlen(s1) + ft_strlen(s2);
-	str = ft_strnew_static(len);
+	str = malloc(sizeof(*str) * (len + 1));
 	ptr = str;
 	if (str == NULL)
 		return (NULL);
